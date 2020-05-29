34217d18811a26b594619097a2a0d3b41b658192
ed-val2 <e.valbuena.d@gmail.com>
2020-05-29 12:29:14 -0500

    unused packaging

--- config/dev.js
+++ config/dev.js
@@ -5,8 +5,8 @@ module.exports = {
   googleClientSecret: '6D4rBADYmImreryA-CEdyA0P',
   mongoURI: 'mongodb+srv://eval:3l3anor@emaily-2aubb.mongodb.net/test?retryWrites=true',
   cookieKey: '34rh7xfbr6sgr5hjf7u5s6iw5ow-',
-  stripePublishableKey: 'pk_test_BgYp383iH9iIQfcsYF6QkhSE00dpy9clYV',
-  stripeSecretKey: 'sk_test_r9EDUrVX0RbdIFl5ahydDtuA00HcTQUQCq',
-  sendGridKey: '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',
-  redirectDomain: 'http://localhost:3000'
+  // stripePublishableKey: 'pk_test_BgYp383iH9iIQfcsYF6QkhSE00dpy9clYV',
+  // stripeSecretKey: 'sk_test_r9EDUrVX0RbdIFl5ahydDtuA00HcTQUQCq',
+  // sendGridKey: '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',
+  // redirectDomain: 'http://localhost:3000'
 };
--- sendgrid_webhook.sh
+++
@@ -1,7 +0,0 @@
-localtunnel (){
-  ssh -R wzextycugae5sdtufy:80:localhost:5000 serveo.net
-}
-until localtunnel; do
-echo "serveo.net server crashed"
-sleep 2
-done
