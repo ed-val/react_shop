5ee302fe3cbedec3f11b796a7f8c68d12c407292
ed-val2 <e.valbuena.d@gmail.com>
2020-05-29 03:30:12 -0500

    removed unnesseray code and added api call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,65 +1,34 @@
-import axios from 'axios';
-import Helpers from './helpers';
-
-import { BASE_API, NEWS_TOKEN } from './config';
+import { BASE_API } from './config';
 
 class Api {
 
-  async getNewsByKeyword(keyword) {
+  async getOrder() {
     let result = null;
     try {
-      const res = await axios({
-        method: 'GET',
-        url: `${BASE_API}/v2/everything?q=${keyword}&sortBy=popularity&apiKey=${NEWS_TOKEN}`
-      });
-      
-      console.log(res);
+      let res = await fetch(`${BASE_API}/api/v2/orders/2117155815564`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: process.env.REACT_APP_API_TOKEN,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
-      // if (!!resDetails.data) {
-      //   const resCharacteristics = await axios({
-      //     method: 'GET',
-      //     url: `${BASE_API}/api/v2/pokemon-species/${resDetails.data.id}`
-      //   });
+      const dataString = await res.text();
+      const data = JSON.parse(dataString);
       
-      //   const pokemon = Helpers.buildPokemon(
-      //     resDetails.data,
-      //     resCharacteristics.data
-      //   );
-      //   result = { error: false, pokemon };
-      // } else {
-      //   result = { error: true, errorMsg: 'No hay resultados' };
-      // }
-        
-      } catch (e) {
-        console.log(e);
-        if (e.message.match(/Network request failed/)) {
-          result = { error: true, errorMsg: 'Error al conectarse con el servidor.' };
-        } else {
-          // por ahora tratar todos los errores con un mensaje generico
-          result = { error: true, errorMsg: 'Error al conectarse con el servidor.' };
+
+      if (data.success) {
+        result = {
+          items: data.order.items,
+          number: data.order.number,
+          currency: data.order.currency,
+          error: false,
         }
       }
 
-    return result;
-  }
-
-  async getNewsfeed() {
-    let result = null;
-    try {
-      const res = await axios({
-        method: 'GET',
-        url: `${BASE_API}/v2/top-headlines?country=us&apiKey=${NEWS_TOKEN}`
-      });
-
-      console.log(res);
-
-      // if (!!resDetails.data) {
-
-      //   result = { error: false, pokemon };
-      // } else {
-      //   result = { error: true, errorMsg: 'No hay resultados' };
-      // }
-        
       } catch (e) {
         console.log(e);
         if (e.message.match(/Network request failed/)) {
