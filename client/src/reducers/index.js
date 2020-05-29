7a63e618ec3083c2464dc410c30048c32c685fb6
ed-val2 <e.valbuena.d@gmail.com>
2020-05-29 03:32:00 -0500

    new stores

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import headerReducer from './headerReducer';
+import homeReducer from './homeReducer';
+import { reducer as reduxForm } from 'redux-form'; //<- this syntax is really useful
 
 export default combineReducers({
   auth: authReducer,
   header: headerReducer,
+  home: homeReducer,
+  form: reduxForm,
 });
