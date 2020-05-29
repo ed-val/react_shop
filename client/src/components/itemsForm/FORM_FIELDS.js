19aa579311a83ec73b80ce2f8b8edabeb3899762
ed-val2 <e.valbuena.d@gmail.com>
2020-05-29 03:31:05 -0500

    form model

---
+++ client/src/components/itemsForm/FORM_FIELDS.js
@@ -0,0 +1,5 @@
+export default [
+  { label: 'Item Name', name: 'name', type: 'select' },
+  { label: 'Quantity', name: 'quantity', type: 'number' },
+  // { label: 'Price', name: 'price', type: 'number' },
+];
